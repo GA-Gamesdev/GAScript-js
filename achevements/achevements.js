5f08e63db23c720dc5b9031aacb2b589fc985240
GA-Gamesdev <helpme704586@gmail.com>
2026-02-14 17:06:50 -0500

    Update achevements.js

--- achevements/achevements.js
+++ achevements/achevements.js
@@ -12,5 +12,37 @@ SAVETO : null,
     }
   console.log("achevements.js loaded")
   }
+
+  grantAchevement(string,string3){
+//string is achevement name
+  //string3 is the achevementID
+  //soon there will be icon for html achevement stuff(when that happens...)
+    localStorage(string,1)
+
+  showToast(title, description){
+    // Create toast element
+    const toast = document.createElement('div');
+    toast.className = 'achievement-toast';
+    toast.innerHTML = `
+      <div class="achievement-icon">🏆</div>
+      <div class="achievement-content">
+        <div class="achievement-title">${title}</div>
+        <div class="achievement-desc">${description}</div>
+      </div>
+    `;
+    
+    // Add to page
+    document.body.appendChild(toast);
+    
+    // Animate in
+    setTimeout(() => toast.classList.add('show'), 100);
+    
+    // Remove after 4 seconds
+    setTimeout(() => {
+      toast.classList.remove('show');
+      setTimeout(() => toast.remove(), 300);
+    }, 4000);
+  }
+  }
   
 }
