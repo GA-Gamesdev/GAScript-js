ca31b05cc4eeca921b499958c1499bf4dfacdbbb
GA-Gamesdev <helpme704586@gmail.com>
2026-02-14 16:14:06 -0500

    Update achevements.js

--- achevements/achevements.js
+++ achevements/achevements.js
@@ -1,3 +1,15 @@
 function acheve(){
+  function load(string2){
+    if(string2==="memory"){
+      let SAVETO = MEMORY
+        console.warn("achevements.js will not save achevement's on memory mode")
+      console.log("achevement.js set to MEMORY")
+    }
+        if(string2==="localStorage"){
+      let SAVETO = LOCALSTORAGE
+          console.log("achevement.js set to LOCALSTORAGE")
+    }
   console.log("achevements.js loaded")
+  }
+  
 }
